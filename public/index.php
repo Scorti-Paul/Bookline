8256a33149794c81a0844a521301cfb71f38b331
Paul Scorti <scortip@gmail.com>
2020-01-07 01:00:08 +0000

    Added content and styled the landing page: "index"

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bookline Web app</title>
+    <link rel="stylesheet" href="main.css">
+
+    <head>
+
+    <body>
+        <main>
+            <section id="secleft">
+                <h2>Bookline Wep App</h2>
+                <p>My books are my knowledge. It's important to give them safe...</p>
+            </section>
+
+            <section id="secright">
+                <img src="../assets/images/phile.svg" alt="Woman reading book">
+            </section>
+        </main>
+    </body>
+
+</html>
