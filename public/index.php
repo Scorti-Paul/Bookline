d50e6079b19b98f2d9081ea582b031c0732dedf4
Paul Scorti <scortip@gmail.com>
2020-01-08 13:19:22 +0000

    navbar working

--- public/index.php
+++ public/index.php
@@ -7,10 +7,27 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bookline Web app</title>
     <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <head>
 
     <body>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="" alt="">
+                        </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li><a href="#">Library</a></li>
+                    <li><a href="#">Discover</a></li>
+                    <li><a href="#">Sign in</a></li>
+                </ul>
+            </nav>
+        </header>
         <main>
             <section id="secleft">
                 <h2>Bookline Wep App</h2>
@@ -22,5 +39,12 @@
             </section>
         </main>
     </body>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </html>
